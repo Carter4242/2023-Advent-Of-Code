b148dfc5786d6726a8a6bef48caf391da1a00563
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2023-12-02 01:04:13 -0500

    Day 2 Done

--- day2/2-1-.py
+++ day2/2-1-.py
@@ -2,3 +2,31 @@ lines = []
 with open("day2/day2.txt", 'r') as f:
     lines = f.readlines()
 
+sum = 0
+for l in lines:
+    l = l.strip('\n').split(" ")
+    gameNum = l[1][:-1]
+    l = l[2:]
+    l = " ".join(l)
+    l = l.split(";")
+    invalid = False
+    for round in l:
+        dice = round.split(',')
+        for d in dice:
+            values = d.strip().split(' ')
+            if values[1] == "blue" and int(values[0]) > 14:
+                invalid = True
+                break
+            if values[1] == "green" and int(values[0]) > 13:
+                invalid = True
+                break
+            if values[1] == "red" and int(values[0]) > 12:
+                invalid = True
+                break
+        if invalid:
+            break
+    if not invalid:
+        print(gameNum)
+        sum += int(gameNum)
+    
+print(sum)
--- day2/2-2-.py
+++ day2/2-2-.py
@@ -2,3 +2,27 @@ lines = []
 with open("day2/day2.txt", 'r') as f:
     lines = f.readlines()
 
+sum = 0
+for l in lines:
+    l = l.strip('\n').split(" ")
+    gameNum = l[1][:-1]
+    l = l[2:]
+    l = " ".join(l)
+    l = l.split(";")
+    maxRed, maxBlue, maxGreen = 0, 0, 0
+    for round in l:
+        dice = round.split(',')
+        for d in dice:
+            values = d.strip().split(' ')
+            if values[1] == "blue":
+                if int(values[0]) > maxBlue:
+                    maxBlue = int(values[0])
+            if values[1] == "green":
+                if int(values[0]) > maxGreen:
+                    maxGreen = int(values[0])
+            if values[1] == "red":
+                if int(values[0]) > maxRed:
+                    maxRed = int(values[0])
+    sum += maxRed*maxGreen*maxBlue
+
+print(sum)
