6a58744f66564ba9fd4a357ce034fa73df1affe0
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2023-12-09 00:42:37 -0500

    Day 9 Done!

--- day9/9-2-.py
+++
@@ -1,3 +0,0 @@
-lines = []
-with open("day9/day9", 'r') as f:
-    lines = f.readlines()
---
+++ day9/9-2-ListDifferencesPredictionOpposite.py
@@ -0,0 +1,23 @@
+lines = []
+with open("day9/day9", 'r') as f:
+    lines = f.readlines()
+
+sum = 0
+traceBack = []
+for l in lines:
+    value = 0
+    l = l.strip().split()
+    traceBack = []
+    traceBack.append(l)
+    endValue = l[-1]
+    while endValue != 0: # just checking the end value doesn't work for 0 1 2 2 (but not case like this exists)
+        newL = []
+        for i in range(1, len(l)):
+            newL.append(int(l[i]) - int(l[i-1]))
+        traceBack.append(newL)
+        l = newL
+        endValue = l[-1]
+    for i in range(len(traceBack)-1, -1, -1):
+        value = int(traceBack[i][0]) - value
+    sum += value
+print(sum)
